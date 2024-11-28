a67e41a39b21c36030102887359d469878fc6d35
jinkjonks <jennyli12320@gmail.com>
2024-11-29 01:18:13 +1100

    save func

--- pringle.py
+++ pringle.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) Vispy Development Team. All Rights Reserved.
-# Distributed under the (new) BSD License. See LICENSE.txt for more info.
-
-
 from functools import partial
 import numpy as np
 
@@ -18,19 +13,6 @@ NUM_LINE_POINTS = 1000
 A = -15
 B = 15
 T = np.linspace(A, B, NUM_LINE_POINTS)
-
-
-def create_cylinder(radius, length, center=(0.0, 0.0, 0.0)):
-    """Creates the data of a cylinder oriented along z axis whose center, radius and length are given as inputs
-    Based on the example given at: https://stackoverflow.com/a/49311446/2602319
-    """
-    z = np.linspace(0, length, 100)
-    theta = np.linspace(0, 2 * np.pi, 100)
-    theta_grid, z_grid = np.meshgrid(theta, z)
-    x_grid = radius * np.cos(theta_grid) + center[0]
-    y_grid = radius * np.sin(theta_grid) + center[1]
-    z_grid = z_grid + center[2]
-    return x_grid, y_grid, z_grid
 
 
 def create_hyperbolic_paraboloid(a, b):
@@ -62,4 +44,4 @@ plot = SurfacePlot(x_grid, y_grid, z_grid, color=PRINGLE_COLOUR)
 
 if __name__ == "__main__":
     pringle_can = Shape(plot)
-    pringle_can.canvas.show(visible=True, run=True)
+    pringle_can.show(visible=True, run=True)
--- wrapper.py
+++ wrapper.py
@@ -1,3 +1,4 @@
+from vispy.io import write_png
 from vispy.scene import Node, SceneCanvas
 from vispy.scene.cameras import TurntableCamera
 
@@ -7,13 +8,9 @@ CANVAS_SIZE = (800, 600)  # (width, height)
 NUM_LINE_POINTS = 500
 
 
-class Shape:
+class Shape(SceneCanvas):
     def __init__(self, plot: type[Node]) -> None:
-        self.canvas = SceneCanvas(
-            size=CANVAS_SIZE,
-            vsync=True,
-        )
-
+        super().__init__(size=CANVAS_SIZE, vsync=True)
         self.view = self.canvas.central_widget.add_view()
         camera = TurntableCamera(
             fov=60, elevation=30, azimuth=50, parent=self.view.scene
@@ -23,3 +20,7 @@ class Shape:
         self.view.camera = camera
         # self.view.add(XYZAxis())
         self.view.add(plot)
+
+    def on_close(self, event):
+        img = self.canvas.render()
+        write_png(f"./out/{__module__}.png", img)
